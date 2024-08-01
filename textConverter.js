6ada60b1b314807809e9723c5a27bfb48ece9dc8
scratch-kaisetu <154499503+scratch-kaisetu@users.noreply.github.com>
2024-08-01 17:18:39 +0900

    Update textConverter.js

--- textConverter.js
+++ textConverter.js
@@ -45,6 +45,7 @@ class TextConverter {
     stringToNumber(str) {
         let result = '';
         for (let i = 0; i < str.length; i++) {
+            // 文字のUnicodeコードポイントを取得して5桁の文字列に変換
             result += str.charCodeAt(i).toString(10).padStart(5, '0'); 
         }
         return result;
@@ -52,7 +53,7 @@ class TextConverter {
 
     numberToString(num) {
         let result = '';
-        for (let i = 0; i < num.length; i += 5) { 
+        for (let i = 0; i < num.length; i += 5) { // 各5桁を1つの文字に変換
             let charCode = parseInt(num.substr(i, 5), 10);
             result += String.fromCharCode(charCode);
         }
